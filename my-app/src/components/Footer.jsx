d5e8eed7e0695db36094f2d026156a86d4b90da6
Ashwani Sharma <Ashwanisharma0706@gmail.com>
2026-04-22 13:45:21 +0530

    Fix footer anchor a11y warnings

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -7,6 +7,7 @@ const Footer = () => {
     <footer className="footer">
       <div className="container">
         <div className="footer-grid">
+          {/* Brand */}
           <div className="footer-brand">
             <div className="footer-logo-wrap">
               <img src="/images/logo/eatzo-logo.png" alt="Eatzo Logo" />
@@ -17,46 +18,113 @@ const Footer = () => {
               favorite restaurants and get your meals delivered quickly.
             </p>
             <div className="footer-socials">
-              <a href="#" className="social-icon">📸</a>
-              <a href="#" className="social-icon">🐦</a>
-              <a href="#" className="social-icon">📘</a>
-              <a href="#" className="social-icon">🎥</a>
+              <a
+                href="https://instagram.com"
+                target="_blank"
+                rel="noreferrer"
+                className="social-icon"
+              >
+                📸
+              </a>
+              <a
+                href="https://twitter.com"
+                target="_blank"
+                rel="noreferrer"
+                className="social-icon"
+              >
+                🐦
+              </a>
+              <a
+                href="https://facebook.com"
+                target="_blank"
+                rel="noreferrer"
+                className="social-icon"
+              >
+                📘
+              </a>
+              <a
+                href="https://youtube.com"
+                target="_blank"
+                rel="noreferrer"
+                className="social-icon"
+              >
+                🎥
+              </a>
             </div>
           </div>
 
+          {/* Company */}
           <div>
             <h4>Company</h4>
             <ul>
-              <li><Link to="/about">About Us</Link></li>
-              <li><Link to="/team">Join the Team</Link></li>
-              <li><Link to="/franchise">Franchise Partners</Link></li>
-              <li><Link to="/blog">Food Blog</Link></li>
-              <li><Link to="/impact">Social Impact</Link></li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/team">Join the Team</Link>
+              </li>
+              <li>
+                <Link to="/franchise">Franchise Partners</Link>
+              </li>
+              <li>
+                <Link to="/blog">Food Blog</Link>
+              </li>
+              <li>
+                <Link to="/impact">Social Impact</Link>
+              </li>
             </ul>
           </div>
 
+          {/* Support */}
           <div>
             <h4>Support</h4>
             <ul>
-              <li><Link to="/contact">Contact Support</Link></li>
-              <li><Link to="/help">Help Center</Link></li>
-              <li><Link to="/safety">Trust & Safety</Link></li>
-              <li><Link to="/restaurants">Partner with Us</Link></li>
-              <li><Link to="/login">Account Login</Link></li>
+              <li>
+                <Link to="/contact">Contact Support</Link>
+              </li>
+              <li>
+                <Link to="/help">Help Center</Link>
+              </li>
+              <li>
+                <Link to="/safety">Trust &amp; Safety</Link>
+              </li>
+              <li>
+                <Link to="/restaurants">Partner with Us</Link>
+              </li>
+              <li>
+                <Link to="/login">Account Login</Link>
+              </li>
             </ul>
           </div>
 
+          {/* Apps */}
           <div className="footer-apps">
             <h4>Get the App</h4>
             <div className="app-badges">
-              <a href="#" className="app-badge">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg" alt="App Store" />
+              <a
+                href="https://apps.apple.com"
+                target="_blank"
+                rel="noreferrer"
+                className="app-badge"
+              >
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg"
+                  alt="Download on the App Store"
+                />
               </a>
-              <a href="#" className="app-badge">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg" alt="Google Play" />
+              <a
+                href="https://play.google.com"
+                target="_blank"
+                rel="noreferrer"
+                className="app-badge"
+              >
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
+                  alt="Get it on Google Play"
+                />
               </a>
             </div>
-            <p className="app-cta-text">Available on iOS & Android</p>
+            <p className="app-cta-text">Available on iOS &amp; Android</p>
           </div>
         </div>
 
